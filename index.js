90e11f81160392254cfef3fd69821edf08ecadd4
Amen Atikpo <alaskale2024@gmail.com>
2022-06-14 11:09:47 +0000

    find method

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+
+function superbowlWin(record){
+    let final =record.find(record =>record.result==="W");
+    
+        if(final.result==='W'){
+            return final.year;
+        }
+        else{
+            console.log('undefined');
+        }
+
+   
+}
